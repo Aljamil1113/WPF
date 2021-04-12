032787e69f2d591a71c90131f28975671734842e
Aljamil1113 <aarazasaljamil@gmail.com>
2021-04-12 15:47:46 +0800

    Evernote Model

---
+++ Evernote/Evernote/Model/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evernote.Model
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public int NotebookId { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public string FileLocation { get; set; }
+    }
+}
---
+++ Evernote/Evernote/Model/Notebook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evernote.Model
+{
+    public class Notebook
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+    }
+}
