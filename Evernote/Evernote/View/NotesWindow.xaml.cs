462df55d0fa88ce68c01a0a9a62c520ed0b18f1d
Aljamil1113 <aarazasaljamil@gmail.com>
2021-04-13 15:32:18 +0800

    RichTextbox and SpeechRecognition Feature

--- Evernote/Evernote/View/NotesWindow.xaml.cs
+++ Evernote/Evernote/View/NotesWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -27,9 +29,32 @@ namespace Evernote.View
             Application.Current.Shutdown();
         }
 
-        private void SpeechButton_Click(object sender, RoutedEventArgs e)
+        private async void SpeechButton_Click(object sender, RoutedEventArgs e)
         {
+            string region = "southcentralus";
+            string key = "87464598e855488dbe3f5c85b71005f2";
 
+            var speechconfig = SpeechConfig.FromSubscription(key, region);
+            using(var audioConfig = AudioConfig.FromDefaultMicrophoneInput())
+            {
+                using (var recognizer = new SpeechRecognizer(speechconfig, audioConfig))
+                {
+                    var result = await recognizer.RecognizeOnceAsync();
+                    contentRichTextBox.Document.Blocks.Add(new Paragraph(new Run(result.Text)));
+                }
+            }
+        }
+
+        private void contentRichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int amountOfCharacters = (new TextRange(contentRichTextBox.Document.ContentStart, contentRichTextBox.Document.ContentEnd)).Text.Length;
+
+            statusTextBlock.Text = $"Document length: {amountOfCharacters} characters";
+        }
+
+        private void boldButton_Click(object sender, RoutedEventArgs e)
+        {
+            contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontWeightProperty, FontWeights.Bold);
         }
     }
 }
